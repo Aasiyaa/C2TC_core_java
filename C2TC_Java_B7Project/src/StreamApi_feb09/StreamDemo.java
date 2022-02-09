023fe8b7846c69b85f242558835b334e0a0d5886
Aasiyaa <94465115+Aasiyaa@users.noreply.github.com>
2022-02-09 10:09:29 +0530

    first commit

---
+++ C2TC_Java_B7Project/src/StreamApi_feb09/StreamDemo.java
@@ -0,0 +1,24 @@
+package StreamApi_feb09;
+import java.util.*;
+import java.util.stream.Stream;
+public class StreamDemo 
+{
+	public static void main(String[] ars)
+	{
+		Integer[] value=new Integer[] {10,20,30};
+		
+		List<String>ll=new ArrayList<String>();
+		ll.add("java");
+		ll.add("python");
+		ll.add("ruby");
+		
+		System.out.println("array elements");
+		Stream<Integer>mystream=Arrays.stream(value);
+		mystream.forEach(System.out::println);
+		
+		System.out.println("-------------------");
+		System.out.println("list elements");
+		Stream<String> mystream1=ll.stream();
+		mystream1.forEach(System.out::println);
+	}
+}
